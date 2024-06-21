4fb8776871b66765fdbc2770344989a10583dc3f
ivansindacco <56919658+ivansindacco@users.noreply.github.com>
2024-06-21 11:11:57 +0300

    Add files via upload

---
+++ pr5_tmp_3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+class WideFormatPrinter {
+public:
+    void printWide(std::string text) {
+        std::cout << "Printing wide: " << text << std::endl;
+    }
+};
+
+class Printer {
+public:
+    virtual void print(std::string text) = 0;
+};
+
+class SimplePrinter : public Printer {
+public:
+    void print(std::string text) override {
+        std::cout << "Printing: " << text << std::endl;
+    }
+};
+
+class WideToSimplePrinterAdapter : public Printer {
+private:
+    WideFormatPrinter* widePrinter;
+
+public:
+    WideToSimplePrinterAdapter(WideFormatPrinter* printer) : widePrinter(printer) {}
+
+    void print(std::string text) override {
+        widePrinter->printWide(text);
+    }
+};
+
+int main() {
+    SimplePrinter simplePrinter;
+    WideFormatPrinter widePrinter;
+
+    simplePrinter.print("Work");
+
+    WideToSimplePrinterAdapter adapter(&widePrinter);
+    adapter.print("Work!");
+
+    return 0;
+}
